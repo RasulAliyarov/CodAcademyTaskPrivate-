c2d49df2aeb9eb08021a87daf5eab98ad379dac6
RasulAliyarov <rasulaliyarov2002@gmail.com>
2022-12-23 04:51:02 +0400

    show card in api

---
+++ react-northproduct-mui/src/pages/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Navbar/Navbar'
+import Footer from '../components/Footer/Footer'
+
+function About() {
+  return (
+    <>
+      <Navbar />
+      <Footer />
+    </>
+  )
+}
+
+export default About
---
+++ react-northproduct-mui/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Footer from '../components/Footer/Footer'
+import Navbar from '../components/Navbar/Navbar'
+import Card from '../components/Card/Card'
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+      <Card />
+      <Footer />
+
+    </>
+  )
+}
+
+export default Home
