08af5e5763940f32c7a88deab54c9206a56c2cf0
RasulAliyarov <rasulaliyarov2002@gmail.com>
2022-12-23 05:10:50 +0400

    add bread to about

--- react-northproduct-mui/src/pages/About.jsx
+++ react-northproduct-mui/src/pages/About.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
 import Navbar from '../components/Navbar/Navbar'
 import Footer from '../components/Footer/Footer'
-
+import { Link } from 'react-router-dom';
+import { Breadcrumbs, Typography } from '@mui/material';
+import "./About.scss"
 function About() {
   return (
     <>
       <Navbar />
+      <div className='container'>
+        <Breadcrumbs aria-label="breadcrumbs">
+          {['Home', 'About'].map((item) => (
+            <Link
+              onClick={(event) => event.preventDefault()}
+              key={item}
+              underline="hover"
+              color="neutral"
+              fontSize="inherit"
+              
+            >
+              {item}
+            </Link>
+          ))}
+
+          <Typography fontSize="inherit"></Typography>
+        </Breadcrumbs>
+      </div>
       <Footer />
     </>
   )
