4970dbd18cb5c32c6c2ff31fe859adebbc95eb94
RasulAliyarov <rasulaliyarov2002@gmail.com>
2022-12-25 23:27:57 +0400

    countries flags

---
+++ react-countries/src/components/Cards/Cards.jsx
@@ -0,0 +1,55 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import Card from './Card'
+
+function Cards() {
+  const [countries, setCountries] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [searchItem, setSearchitem] = useState("")
+  const [continents, setContinents] = useState("")
+
+  useEffect(() => {
+    axios.get("https://restcountries.com/v3.1/all").then((resp) => {
+      setCountries(resp.data)
+      setLoading(false)
+    })
+  }, [])
+
+  return (
+    <div className="content container">
+      <div className="inputs">
+        <input type="text" onChange={(e) => setSearchitem(e.target.value)} placeholder='Search for country' />
+
+        <select name="continents" onChange={(e) => setContinents(e.target.value)} id="continents">
+          <option value="africa">Choose continets</option>
+          <option value="africa">Africa</option>
+          <option value="america">America</option>
+          <option value="asia">Asia</option>
+          <option value="europe">Europe</option>
+          <option value="oceania">Oceania</option>
+        </select>
+      </div>
+      <div className='cards'>
+
+        {loading && <span> Loading...</span>}
+        {
+          countries.filter((value) => {
+            if (value === "") {
+              console.log("def")
+              return value
+            }
+            else if (value.name.common.toLowerCase().includes(searchItem.toLowerCase()) && value.region.toLowerCase().includes(continents.toLowerCase())) {
+              console.log("inp")
+              return value
+            }
+          }).map((country) => {
+
+            return <Card key={country?.flag} country={country} />
+          })
+        }
+      </div >
+    </div>
+  )
+}
+
+export default Cards
---
+++ react-countries/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Inputs from '../components/Inputs/Inputs'
+import Cards from '../components/Cards/Cards'
+
+function Home() {
+  return (
+    <>
+      <Inputs/>
+      <Cards />
+    </>
+  )
+}
+
+export default Home
